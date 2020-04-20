b1aa5608b5a109a6c61753e19d102644cf518a1d
Joga Nikhitha Rani <joganikhitharani@gmail.com>
2020-04-20 17:43:05 +0530

    first

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.styl']
+})
+export class AboutComponent implements OnInit {
+
+  ImgUrl='https://pixabay.com/photos/iron-man-avengers-marvel-super-hero-4241268https://cdn.pixabay.com/photo/2019/05/31/02/08/iron-man-4241268_960_720.jpg/'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
